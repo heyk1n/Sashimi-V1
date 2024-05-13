371504b3a7d6a61b14d7ec83b938f1ce0c9acf52
Kin <riennsuu@gmail.com>
2024-05-13 11:27:12 +0000

    move directory

--- src/test.ts
+++
@@ -1,7 +0,0 @@
-const response = await fetch(
-	"https://raw.githubusercontent.com/heyk1n/sashimi-oauth-test/prod/src/static/babydoll.ttf",
-);
-await Deno.writeFile(
-	"static/babydoll.ttf",
-	new Uint8Array(await response.arrayBuffer()),
-);
