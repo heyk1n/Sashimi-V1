328d1dae8fc0df60ed14c724ffda9a4cfde44c8b
Kin <riennsuu@gmail.com>
2024-05-23 07:43:52 +0000

    unauthorized user page

--- routes/index.tsx
+++ routes/index.tsx
@@ -80,54 +80,56 @@ export const handler = helpers.defineHandlers({
 
 export default helpers.definePage<typeof handler>(({ data }) => {
 	const { authorizeUrl, code, user } = data;
-	if (!user) {
-		return <p>You're not logged in</p>;
-	} else {
-		return (
-			<div class="w-dvw h-dvh bg-white font-babydoll">
-				<div class="h-full w-full relative">
-					<div class="w-full h-full grid place-items-center absolute">
-						<div class="bg-white w-80 shadow-xl rounded-xl overflow-hidden">
-							<img class="pointer-events-none" src="/sashimi.jpg">
-							</img>
-							<div class="bg-white p-6 space-y-3">
-								<div class="select-none space-y-1 grid place-items-center text-center">
-									<p class="text-2xl font-semibold">
-										Welcome!!
-									</p>
-									<p class="text-gray-600 text-sm">
-										Sebelum bergabung, verifikasi akun kamu
-										terlebih dahulu ya! {">~<"}
-									</p>
-								</div>
-								{user
-									? (
-										<form method="POST" action="/">
-											<input
-												type="text"
-												name="code"
-												class="bg-gray-200 text-center rounded-lg w-full h-10"
-												placeholder="kitsunee"
-												value={code ?? ""}
-											>
-											</input>
-										</form>
-									)
-									: <a href={""}></a>}
+	return (
+		<div class="w-dvw h-dvh bg-white font-babydoll">
+			<div class="h-full w-full relative">
+				<div class="w-full h-full grid place-items-center absolute">
+					<div class="bg-white w-80 shadow-xl rounded-xl overflow-hidden">
+						<img class="pointer-events-none" src="/sashimi.jpg">
+						</img>
+						<div class="bg-white p-6 space-y-3">
+							<div class="select-none space-y-1 grid place-items-center text-center">
+								<p class="text-2xl font-semibold">
+									Welcome!!
+								</p>
+								<p class="text-gray-600 text-sm">
+									Sebelum bergabung, verifikasi akun kamu
+									terlebih dahulu ya! {">~<"}
+								</p>
 							</div>
+							{user
+								? (
+									<form method="POST" action="/">
+										<input
+											type="text"
+											name="code"
+											class="bg-gray-200 text-center rounded-lg w-full h-10"
+											placeholder="kitsunee"
+											value={code ?? ""}
+										>
+										</input>
+									</form>
+								)
+								: (
+									<div class="w-full grid place-items-center">
+										<a href={authorizeUrl}>
+											<p class="bg-yellow-200 px-4 py-2 rounded-full select-none">Login with Discord</p>
+										</a>
+									</div>
+								)}
 						</div>
 					</div>
-					{user && (
-						<div class="absolute top-8 right-8">
-							<Menu
-								avatar={user.avatar}
-								username={user.username}
-							>
-							</Menu>
-						</div>
-					)}
 				</div>
+				{user && (
+					<div class="absolute top-8 right-8">
+						<Menu
+							avatar={user.avatar}
+							username={user.username}
+						>
+						</Menu>
+					</div>
+				)}
 			</div>
-		);
-	}
+		</div>
+	);
 });
